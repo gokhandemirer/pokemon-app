0ff4a0cfb420252d35ccd003f10f108d59084018
Gökhan Demirer <gdmr34@gmail.com>
2024-04-23 18:02:52 +0300

    update page.tsx

--- src/app/pokemon/page.tsx
+++ src/app/pokemon/page.tsx
@@ -5,6 +5,7 @@ import PokemonList from "@/feature/Pokemon/PokemonList";
 import Loader from "@/components/UI/Loader";
 import { Pokemon } from "@/types";
 import Header from "@/components/UI/Layout/Header";
+import Link from "next/link";
 
 export default function PokemonListPage() {
   const router = useRouter();
@@ -32,7 +33,14 @@ export default function PokemonListPage() {
 
   return (
     <>
-      <Header title="Pokemon List" />
+      <Header title="Pokemon List">
+        <Link
+          href={"/"}
+          className="border border-gray-300 bg-white px-3 py-2 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+        >
+          Go to Home
+        </Link>
+      </Header>
       <div className="py-4"></div>
       <main className="w-8/12 mx-auto">
         <PokemonList
