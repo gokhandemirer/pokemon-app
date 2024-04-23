368d7bbd9e1867f7d77f80c470475b40cf7be1d9
Gökhan Demirer <gdmr34@gmail.com>
2024-04-23 12:13:06 +0300

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    API_URL: process.env.API_URL,
+    FETCH_LIMIT: process.env.FETCH_LIMIT,
+  },
+};
 
 export default nextConfig;
