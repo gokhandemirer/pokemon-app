4bfd26b1965d7c318026323d167acb42f62cf845
Gökhan Demirer <gdmr34@gmail.com>
2024-04-23 15:16:43 +0300

    Add PokemonDetailPage component and PokemonCard component

---
+++ src/app/pokemon/[name]/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { notFound } from "next/navigation";
+import Loader from "@/components/UI/Loader";
+import PokemonCard from "@/feature/Pokemon/PokemonCard";
+import useGetPokemonDetail from "@/hooks/useGetPokemonDetail";
+import Header from "@/components/UI/Layout/Header";
+
+type PokemonDetailPageProps = {
+  name: string;
+};
+
+export default function PokemonDetailPage({
+  params,
+}: {
+  params: PokemonDetailPageProps;
+}) {
+  const { name } = params;
+  const { pokemon, loading, pokemonNotFound } = useGetPokemonDetail(name);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (pokemonNotFound) {
+    return notFound();
+  }
+
+  return (
+    <>
+      <Header title="Pokemon Detail" />
+      <div className="py-4"></div>
+      <main>
+        <div className="flex justify-center">
+          <div className="flex w-1/2">
+            <PokemonCard pokemon={pokemon!} />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ src/feature/Pokemon/PokemonCard.tsx
@@ -0,0 +1,56 @@
+import { useRouter } from "next/navigation";
+import { PokemonDetail } from "@/types";
+
+type PokemonCardProps = {
+  pokemon: PokemonDetail;
+};
+
+function InfoLine({ title, value }: { title: string; value: string | number }) {
+  return (
+    <p>
+      <strong>{title}:</strong> {value}
+    </p>
+  );
+}
+
+export default function PokemonCard({ pokemon }: PokemonCardProps) {
+  const router = useRouter();
+
+  return (
+    <>
+      <div className="bg-white rounded-lg shadow-md p-4 mb-4 w-full">
+        <div className="flex flex-col mb-4 text-2xl gap-2">
+          <button
+            className="text-sm cursor-pointer hover:bg-gray-200 rounded-md text-left"
+            style={{ width: "fit-content" }}
+            onClick={() => router.push("/pokemon")}
+          >
+            Back to list
+          </button>
+          <span>{pokemon.name}</span>
+        </div>
+        <div className="flex flex-col mb-4">
+          <InfoLine title="ID" value={pokemon.id} />
+          <InfoLine title="Name" value={pokemon.name} />
+          <InfoLine title="Height" value={pokemon.height} />
+          <InfoLine title="Weight" value={pokemon.weight} />
+          <InfoLine title="Base Experience" value={pokemon.base_experience} />
+          <InfoLine title="Order" value={pokemon.order} />
+          <InfoLine
+            title="Is Default"
+            value={pokemon.is_default ? "Yes" : "No"}
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <strong>Abilities:</strong>
+          <ul>
+            {pokemon.abilities.map((ability) => (
+              <li key={ability.ability.name}>{ability.ability.name}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
