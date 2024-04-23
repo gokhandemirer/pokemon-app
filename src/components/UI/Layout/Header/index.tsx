6d7f993995302498f6a08310722834db1517610c
Gökhan Demirer <gdmr34@gmail.com>
2024-04-23 18:02:47 +0300

    update src/components/UI/Layout/Header/index.tsx

--- src/components/UI/Layout/Header/index.tsx
+++ src/components/UI/Layout/Header/index.tsx
@@ -1,12 +1,14 @@
 interface HeaderProps {
   title: string;
+  children?: React.ReactNode;
 }
 
-export default function Header({ title }: HeaderProps) {
+export default function Header({ title, children }: HeaderProps) {
   return (
     <header>
       <div className="flex justify-between items-center px-3 py-5 bg-gray-50 border-b-gray-200 border-b">
         <h1>{title}</h1>
+        {children}
       </div>
     </header>
   );
